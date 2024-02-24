a07fe22840b061ff85ee1ddb894b9f89fa30a775
uttam-ops <138990224+uttam-ops@users.noreply.github.com>
2024-02-24 21:14:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-21"
+    bucket = "terraform-pipeline-project"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
